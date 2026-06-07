9aa81e99ba6c14ac0223a761ccbfc3c5fcab7843
sajib689 <sajibbabu751@gmail.com>
2026-06-07 21:33:42 +0600

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "server is running...",
+    environment: process.env.NODE_ENV,
+    uptime: process.uptime().toFixed(2) + " seconds",
+    timestamp: new Date().toISOString(),
+  });
 });
 
+// Global error handling and 404 not found middleware should be registered after all routes
 
 app.use(globalErrorHandler);
-
 app.use(notFound);
 
 export default app;
