ab49380fc65fd447b59f56cb1da15ce47d7a4781
sajib689 <sajibbabu751@gmail.com>
2026-06-09 20:09:15 +0600

    added: hashPassword

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,15 @@
 import { createPatientInput } from "./user.interface";
+import bcrypt from "bcrypt";
 
 const createPatient = async (payload: createPatientInput) => {
-    const result =  await 
+  const hashPassword = await bcrypt.hash(payload.password, 10);
+
+  const result = {
+    ...payload,
+    password: hashPassword,
+  };
+  return result;
+  
 };
 
 export const userService = {
