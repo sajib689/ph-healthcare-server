2f9c024c07288b1b89795efde83a6d1f6e22b990
sajib689 <sajibbabu751@gmail.com>
2026-06-10 19:59:11 +0600

    validation request middleware  added for the zod

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+
+const createPatientValidationSchema = z.object({
+  password: z.string(),
+  patient: {
+    name: z.string({
+      error: "Name is required",
+    }),
+    email: z.string({
+      error: "Email is required",
+    }),
+    address: z.string().optional(),
+  },
+});
+
+
+export const UserValidation = {
+    createPatientValidationSchema
+}
