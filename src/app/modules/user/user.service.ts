3c3397910341e3d435240f75d3fea69a64baedb8
sajib689 <sajibbabu751@gmail.com>
2026-06-10 17:55:57 +0600

    prisma error solved

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,19 @@
+import { prisma } from "../../shared/prisma";
 import { createPatientInput } from "./user.interface";
 import bcrypt from "bcrypt";
 
 const createPatient = async (payload: createPatientInput) => {
   const hashPassword = await bcrypt.hash(payload.password, 10);
 
-  const result = {
-    ...payload,
-    password: hashPassword,
-  };
-  return result;
-  
+  const result = await prisma.$transaction(async (tnx) => {
+    await tnx.user.create({
+      data: {
+        email: payload.email,
+        password: hashPassword,
+      },
+    });
+    
+  });
 };
 
 export const userService = {
