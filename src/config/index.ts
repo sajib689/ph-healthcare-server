68b21027980dd4082aec11fb6e520a782ef840d6
sajib689 <sajibbabu751@gmail.com>
2026-06-14 19:45:38 +0600

    üpdate image

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,15 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-}
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
+};
