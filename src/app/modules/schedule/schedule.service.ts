0103f3e391aed761583eb2b44e1c3cb633535085
sajib689 <sajibbabu751@gmail.com>
2026-07-07 22:58:19 +0600

    added: integrate date-fns for date manipulation in schedule service

--- src/app/modules/schedule/schedule.service.ts
+++ src/app/modules/schedule/schedule.service.ts
@@ -1,7 +1,35 @@
+import { addMinutes, addHours, format } from "date-fns";
+
 const insertIntoDb = async (payload: any) => {
-  console.log("Payload received in service:", payload);
-  // Here you would typically insert the payload into your database.
-  // For demonstration purposes, we'll just return the payload.
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const intervalTime = 30;
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0]),
+        ),
+        Number(startTime.split(":")[1]),
+      ),
+    );
+  
+    const endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(lastDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0]),
+        ),
+        Number(endTime.split(":")[1]),
+      ),
+    );
+  }
+
   return payload;
 };
 
