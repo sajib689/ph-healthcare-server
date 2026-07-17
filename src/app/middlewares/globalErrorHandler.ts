4728a51c5c6b3f4519479c3739eeaab72d6e9e9d
sajib689 <sajibbabu751@gmail.com>
2026-07-17 18:53:24 +0600

    refactor: improve formatting and structure of global error handler for better readability

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,36 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    let success = false;
-    let message = err.message || "Something went wrong!";
-    let error = err;
-
-    res.status(statusCode).json({
-        success,
-        message,
-        error
-    })
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      ((message = "Duplicate key error!"), (error = err.meta));
+    }
+    if (err.code === "P1000") {
+      ((message = "Authentication failed server error"), (error = err.meta));
+    }
+    if (err.code === "P2003") {
+      ((message = "Foreign key constraint failed on the field"),
+        (error = err.meta));
+    }
+    
+  }
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
 };
 
 export default globalErrorHandler;
