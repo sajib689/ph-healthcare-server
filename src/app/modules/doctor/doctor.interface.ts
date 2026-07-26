6f020983ba381d35c033ac94abf7cb9b240fc6df
sajib689 <sajibbabu751@gmail.com>
2026-07-26 18:39:55 +0600

    feat: update IDoctorUpdateInput type and enhance updateDoctor functionality

--- src/app/modules/doctor/doctor.interface.ts
+++ src/app/modules/doctor/doctor.interface.ts
@@ -13,8 +13,10 @@ export type IDoctorUpdateInput = {
   currentWorkingPlace: string;
   designation: string;
   isDeleted: boolean;
-  specialties: {
-    specialtyId: string;
-    isDeleted?: boolean;
-  };
+  specialties: [
+    {
+      specialtyId: string;
+      isDeleted?: boolean;
+    },
+  ];
 };
--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -16,12 +16,12 @@ const getFromDb = async (filters: any, options: IOptions) => {
   if (searchTerm) {
     andCondition.push({
       OR: doctorSearchableFields.map((filed) => ({
-      [filed]: {
-        contains: searchTerm,
-        mode: "insensitive",
-      },
-    }))
-    })
+        [filed]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
   }
 
   //   filter wise work
@@ -35,52 +35,91 @@ const getFromDb = async (filters: any, options: IOptions) => {
   }
 
   const whereCondition: Prisma.DoctorWhereInput =
-    andCondition.length > 0 ? { AND: andCondition } : {}
+    andCondition.length > 0 ? { AND: andCondition } : {};
 
-    const result = await prisma.doctor.findMany({
-      where: whereCondition,
-      skip,
-      take: limit,
-      orderBy: {
-        [sortBy]: sortOrder
-      }
+  const result = await prisma.doctor.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+  });
 
-    })
+  const doctorCount = await prisma.doctor.count({
+    where: whereCondition,
+  });
 
-    const doctorCount = await prisma.doctor.count({
-      where: whereCondition
-    })
-
-    return {
-      meta: {
-        page,
-        limit,
-        total: doctorCount
-      },
-      data: result
-    }
+  return {
+    meta: {
+      page,
+      limit,
+      total: doctorCount,
+    },
+    data: result,
+  };
 };
 
-
-const updateDoctor = async (id: string, payload: Partial<IDoctorUpdateInput>) => {
-  
+const updateDoctor = async (
+  id: string,
+  payload: Partial<IDoctorUpdateInput>,
+) => {
   const doctorInfo = await prisma.doctor.findFirstOrThrow({
     where: {
-      id
+      id,
+    },
+  });
+  const { specialties, ...doctorData } = payload;
+
+  if (specialties && specialties.length > 0) {
+    const deleteSpecialtiesIds = specialties.filter(
+      (specialty) => specialty.isDeleted,
+    );
+
+    // deleted the dr specialty using the for loop
+    for (const specialty of deleteSpecialtiesIds) {
+      await prisma.doctorSpecialties.deleteMany({
+        where: {
+          doctorId: id,
+          specialtiesId: specialty.specialtyId,
+        },
+      });
     }
-  })
+    // for the create specialtiesId first filter the data then using
+    // the for loop create
+    
+    const createSpecialtyIds = specialties.filter(
+      (specialty) => !specialty.isDeleted,
+    );
+
+    for (const specialty of createSpecialtyIds) {
+      await prisma.doctorSpecialties.create({
+        data: {
+          doctorId: id,
+          specialtiesId: specialty.specialtyId,
+        },
+      });
+    }
+  }
 
   const result = await prisma.doctor.update({
     where: {
-      id: doctorInfo.id
+      id: doctorInfo.id,
     },
-    data: payload
-  })
+    data: doctorData,
+    include: {
+      doctorSpecialties: {
+        include: {
+          specialties: true,
+        },
+      },
+    },
+  });
 
-  return result
-}
+  return result;
+};
 
 export const DoctorService = {
   getFromDb,
-  updateDoctor
+  updateDoctor,
 };
