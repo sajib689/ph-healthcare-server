43c2fc021d88cbdbeee60552e99a4dc6715d3dd1
sajib689 <sajibbabu751@gmail.com>
2026-07-30 08:49:24 +0600

    feat: add appointment model with relationships, update user and schedule models, and implement appointment service

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
+import { AppointmentService } from "./appointment.service";
+import sendResponse from "../../shared/sendResponse";
+import  httpStatus  from 'http-status';
 
 const insertAppointments = catchAsync(async (req: Request, res: Response) => {
 
+    const result = await AppointmentService.insertAppointments(req.body)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Appointment created successfully!",
+        data: result
+    })
 })
 
 export const AppointmentsController = {
--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,5 +1,5 @@
 
-const insertAppointments = () => {
+const insertAppointments = (payload) => {
 
 }
 
